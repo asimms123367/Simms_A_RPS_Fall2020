799b1496bc9d1ac6ec07cd4c4e7a1e26f0311f77
Andenis <andenis.simms@gmail.com>
2020-11-24 13:43:18 -0500

    setting up packages

--- game.py
+++ game.py
@@ -1,123 +1,78 @@
 #inport packages to extend (just like we extend sublime, or Atom or  VS code)
 from random import randint
 
-#[] => this is an array. An arayis a special  tyep of container  that can hold multiple items\
-# arrays are indexed (their content get assigned a number)
-# the index always starts at 0
-choices =["rock", "paper", "scissors"]
+#re-import our game variables
+from gameComponents import gameVars, winLose
 
 
-
-
-player_lives = 1
-computer_lives = 1
-
-total_lives = 1
-
-player = False # True and False are Booleans - data types that can be truthy or falsey
-
-def winorlose(status):
-	#print ("called winorlose', status)
-
-	if status == "won":
-		pre_message = "You are yuuuuugest winner ever!"
-
-	else:
-
-		pre_message = "You done trumped it, loser!"
-
-	print(pre_message + " Would you like to play again?")
-
-
-	choice = input ("Y / N?")
-
-	if choice== "Y" or choice =="y":
-
-		global player_lives
-		global computer_lives
-		global player
-		#reset the game and start over
-		player_lives = 1
-		computer_lives = 1
-		player = False
-
-	elif choice =="N" or choice == "n":
-			#exit message and quit
-			print ("You chose to quit. Better luck next time!")
-			exit()
-
-	else:
-			print ("Make a valid choice Y or N")
-			#this is generating a bug -> need to fix this check
-			choice = input ("Y / N")
 
 
 
 # set up our game loop
-while player ==  False:
-	#this is the player choice
+while gameVars.gameVars.player ==  False:
+	#this is the gameVars.player choice
 	print ("===============*/ RPS / *===================")
-	print ("Computer Lives:", computer_lives,"/", total_lives)
-	print ("Player Lives:", player_lives,"/", total_lives)
+	print ("Computer Lives:", computer_lives,"/", gameVars.total_lives)
+	print ("gameVars.Player Lives:", gameVars.player_lives,"/", gameVars.total_lives)
 	print ("==================================")
 	print ("Choose your weapon! or type quit to exit\n")
-	player = input ("Choose rock, paper or scissors: \n")
+	gameVars.player = input ("Choose rock, paper or scissors: \n")
 
-	# if the player chooses to quit, thend ont do anything else
+	# if the gameVars.player chooses to quit, thend ont do anything else
 	#just exit the process (kill Python) and quit the game
-	if player == "quit":
+	if gameVars.player == "quit":
 		print ("You chose to quit, quitter!")
 		exit()
 
 	#this will be the AI choice > a random pick grom the choice array
-	computer = choices[randint(0, 2)]
+	computer = gameVars.choices[randint(0, 2)]
 
 	#check to see what the user input
 
-	# print outputs whatever is in the round brackets > in this case it out puts players to the command prompt window
-	print ("user chose: " + player)
+	# print outputs whatever is in the round brackets > in this case it out puts gameVars.players to the command prompt window
+	print ("user chose: " + gameVars.player)
 
 	# validate that the random choice workd for the AI
 	print("AI choice: " + computer)
 
-	if computer == player:
+	if computer == gameVars.player:
 		print("tie")
 
 	elif computer =="rock":
-		if player == "scissors":
-			player_lives = player_lives - 1
-			print("You lose! player lives:", player_lives)
+		if gameVars.player == "scissors":
+			gameVars.player_lives = gameVars.player_lives -=1
+			print("You lose! gameVars.player lives:", gameVars.player_lives)
 		else:
 	   		print("you win!")
-	   		computer_lives = computer_lives -1
+	   		computer_lives = computer_lives -=1
 
 	elif (computer == "paper"):
-		if (player == "scissors"):
-			player_lives = player_lives - 1
-			print("you lose! player lives:",player_lives)
+		if (gameVars.player == "scissors"):
+			gameVars.player_lives = gameVars.player_lives -=1
+			print("you lose! gameVars.player lives:",gameVars.player_lives)
 		else:
 	   		print("you win!")
-	   		computer_lives = computer_lives -1
+	   		computer_lives = computer_lives -=1
 
 	elif (computer == "scissors"):
-		if (player == "rock"):
-			player_lives = player_lives - 1
-			print("you lose! player lives:", player_lives)
+		if (gameVars.player == "rock"):
+			gameVars.player_lives -=1
+			print("you lose! gameVars.player lives:", gameVars.player_lives)
 		else:
 	   		print("you win!")
 	   		computer_lives = computer_lives -1
 
-	# check player lives and AI lives
-	if player_lives == 0:
-		winorlose("lost")
+	# check gameVars.player lives and AI lives
+	if gameVars.player_lives == 0:
+		winLose.winorlose("lost")
 		# print ("You lost! Loser! Would you like to play again?")
 		# choice = input ("Y / N")
 
 		# if choice == "Y" or choice == "y":
 		# 	#reset the game and start over again
-		# 	player_lives = 2
+		# 	gameVars.player_lives = 2
 		# 	computer_lives = 2
-		# 	player = False
+		# 	gameVars.player = False
 
 		# elif choice == "N" or choice =="n":
 		# 	# exit message and quit
@@ -131,7 +86,7 @@ while player ==  False:
 		# 	choice = input ("Y / N")
 
 	elif computer_lives == 0:
-		winorlose("won")
+		wwinLose.inorlose("won")
 		# print ("You won! Winner! Would you like to play again?")
 		# choice = input ("Y / N")
 
